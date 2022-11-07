685edb7c0bea159094f5fea3c7b121ca0ee3826e
Chris Garcia <garciac9203@gmail.com>
2022-11-07 12:23:07 -0800

    Finished react components

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // TODO
+const GroceryList = (props) => (
+  <ul>Grocery List
+    {props.groceries.map((grocery) => (
+      <GroceryListItem grocery ={grocery} />
+    ))}
+  </ul>
+);
+
+const GroceryListItem = (props) => {
+  const [hoveredOn, setHoveredOn ] = React.useState(false);
+  const style = {
+    fontWeight: hoveredOn ? 'bold' : 'normal'
+  };
+  return (
+    <li style={style} onMouseEnter={() => setHoveredOn(true)} onMouseLeave={() => setHoveredOn(false)}>
+      {props.grocery}
+    </li>
+  );
+};
+
+const App = () => (
+  <GroceryList groceries={['Cucumber', 'Kale']} />
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
